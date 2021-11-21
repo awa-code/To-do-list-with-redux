e323b3e64457b9387e615b368b58940aee80c68b
awa-code <66476113+awa-code@users.noreply.github.com>
2021-11-21 23:23:30 +0100

    router change

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import data from './store/reducers/data';
 import { Provider } from 'react-redux';
 import { createStore,applyMiddleware,compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
-import {BrowserRouter,HashRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 
 
 
